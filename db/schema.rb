c89bd35e961376505ce70bf1c343d64542b15dbb
Aditya Tiwari <taditya.tiwari007@gmail.com>
2024-11-15 12:31:14 +0530

    Feat: Add unique index for user email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_13_101106) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_15_065048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -90,6 +90,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_13_101106) do
     t.integer "posts_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
